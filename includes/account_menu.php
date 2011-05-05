58721f73555db284e5d9e658c4fca0afd12ef06e
Paul Chicarello <paul@broadinstitute.org>
2011-05-05 14:05:22 -0400

    * More visual changes focused in these files

--- includes/account_menu.php
+++ includes/account_menu.php
@@ -1,4 +1,5 @@
-<div>Hello <?php echo($_SESSION['userFullName']); ?>! you are now logged in.  Click <a href="?logout">here</a> to logout.</div>
+<div><div>Hello <?php echo($_SESSION['userFullName']); ?>! you are now logged in.  Click <a href="?logout">here</a> to logout.</div>
 <div class="account"><a href="searchjob.php">Search Jobs</a></div>
-<div class="account"><a href="viewall.php">Show All Jobs</a></div>
-<div class="account"><a href="myjobs.php">My Jobs</a></div>
+<?php if ( $_SESSION['userType'] == 'employer'){ ?><div class="account"><a href="postjob.php">Post</a></div><?php } ?>
+<div><a href="viewall.php">Show All Jobs</a></div>
+<div><a href="myjobs.php">My Jobs</a></div></div>
